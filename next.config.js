9f6309bcd64e307f2ada9c4c1f388ab6eaa57352
Sminys <gako3535@gmail.com>
2023-08-10 19:49:39 +0900

    :tada: Start refactor

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['assets.coingecko.com'],
+  },
+  ...(process.env.NODE_ENV === 'production' && {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  }),
+};
+
+module.exports = nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,58 +1,61 @@
-"use client";
+import { Suspense } from 'react';
+import { Fira_Code } from 'next/font/google';
+import cn from 'classnames';
+import { QueryClientProvider } from '@/app/shared/query-client-provider';
+import { ThemeProvider } from '@/app/shared/theme-provider';
+import WagmiConfig from '@/app/shared/wagmi-config';
+import ModalsContainer from '@/components/modal-views/container';
+import DrawersContainer from '@/components/drawer-views/container';
+import SettingsButton from '@/components/settings/settings-button';
+import SettingsDrawer from '@/components/settings/settings-drawer';
+// base css file
+import 'overlayscrollbars/overlayscrollbars.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import '@/assets/css/scrollbar.css';
+import '@/assets/css/globals.css';
+import '@/assets/css/range-slider.css';
 
-import {Footer} from "@/components/Footer"
-import "./globals.css";
-import { Providers } from "./providers";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import Header from "@/components/Header";
-import { RecoilRoot } from "recoil";
-import { Box, ColorModeScript, Container } from "@chakra-ui/react";
-import {Html} from "next/document";
+const fira_code = Fira_Code({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const metadata = {
+  title: 'Criptic',
+  description: 'Criptic - React Next Web3 NFT Crypto Dashboard Template',
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
-
   return (
-    <>
-      <body style={{ minHeight: "100vh" }}>
-        <ColorModeScript initialColorMode="dark" />
-        <ThirdwebProvider
-          authConfig={{
-            // Set this to your domain to prevent signature malleability attacks.
-            domain: process.env.NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN ?? "",
-          }}
-          activeChain={{
-            chainId: 2000777,
-            rpc: [
-              "http://aops-custom-202305-2crvsg-nlb-1d600174371701f9.elb.ap-northeast-2.amazonaws.com:9650/ext/bc/XpX1yGquejU5cma1qERzkHKDh4fsPKs4NttnS1tErigPzugx5/rpc",
-            ],
-            nativeCurrency: {
-              decimals: 18,
-              name: "Avalanche coin",
-              symbol: "AVAX",
-            },
-            shortName: "AVAX",
-            slug: "AVAX",
-            testnet: true,
-            chain: "Glitch",
-            name: "Glitch devnet",
-          }}
-        >
-          <RecoilRoot>
-            <Providers>
-              <Box px={{ sm: "3", md: "5", lg: "10" }} minH="100vh">
-                <Header />
-                {children}
-                <Footer/>
-              </Box>
-            </Providers>
-          </RecoilRoot>
-        </ThirdwebProvider>
+    <html lang="en" dir="ltr" className={cn('light', fira_code.className)}>
+      <head>
+        {/* maximum-scale 1 meta tag need to prevent ios input focus auto zooming */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1 maximum-scale=1"
+        />
+      </head>
+      <body>
+        <QueryClientProvider>
+          <ThemeProvider>
+            <WagmiConfig>
+              <SettingsButton />
+              <SettingsDrawer />
+              <Suspense fallback={null}>
+                <ModalsContainer />
+                <DrawersContainer />
+              </Suspense>
+              {children}
+            </WagmiConfig>
+          </ThemeProvider>
+        </QueryClientProvider>
       </body>
-    </>
+    </html>
   );
 }
